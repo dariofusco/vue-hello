4f928701cb0a47fa34b09ee0bd38aa212d408b99
dariofusco <fdario@hotmail.it>
2023-07-19 14:01:43 +0200

    esegue esercizio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp ({
+    data() {
+        return {
+            message: "Benvenuto!",
+            image: "https://unsplash.it/300/300?image=",
+        }
+    }
+});
+
+app.mount("#app");
